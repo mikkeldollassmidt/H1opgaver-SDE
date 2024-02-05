a5ff2f131322bf6f1c56118d1f1882066efba06b
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-05 12:56:23 +0100

    Uploadet Basic Opgave 6

---
+++ Basic6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Basic6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // user interaction
+            Console.WriteLine("Find cube of:");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            // Use TheCubeOf method to find cube of the users input.
+            Console.WriteLine(TheCubeOf(input));
+            Console.ReadLine();
+        }
+
+        public static double TheCubeOf(double input)
+        {
+            // Cube formular. Input^3 
+            double findCube = input * input * input;
+            return findCube;
+        }
+    }
+}
