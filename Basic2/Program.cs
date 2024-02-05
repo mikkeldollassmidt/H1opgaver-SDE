c138fe75997ff2cd360d972904bfab949d2b3a2c
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-05 11:58:22 +0100

    Uploadet Basic 2

---
+++ Basic2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Basic2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Capture user input in a double.
+            Console.WriteLine("Celsius:");
+            double celsiusInput = Convert.ToDouble(Console.ReadLine());
+
+            // If statement for user input not to be below 271,15
+            if (celsiusInput > -271.15)
+            {
+                // Convert in method. Write result.
+                Console.Clear();
+                Console.WriteLine("T = " + CtoF(celsiusInput) + "F");
+            } else
+            {
+                // Handle if input is below 271,15
+                Console.Clear();
+                Console.WriteLine("Temperature below absolute zero!");
+            }
+        }
+
+        public static double CtoF(double c)
+        {
+            // Convert celsius to fahrenheit formluar
+            double converter = (c * 1.8) + 32;
+
+            // Return converter.
+            return converter;
+        }
+    }
+}
