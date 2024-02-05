7de0f6ade61bc097f0499faa4c92c016b4333525
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-05 14:16:06 +0100

    Uploadet Conditional Opgave 1

---
+++ Conditional1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Conditional1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // User interaction
+            Console.WriteLine("Write number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            // Use method AbsoluteValue to find absolute value from user input.
+            Console.WriteLine("Absolute value: " + AbsoluteValue(input));
+            Console.ReadLine();
+        }
+
+        public static int AbsoluteValue(int input)
+        {
+            // Use Math.Abs to make the input absolute.
+            int absoluteInput = Math.Abs(input);
+            return absoluteInput;
+        }
+    }
+}
