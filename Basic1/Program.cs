3b0ff13103427a5d2b1072cde18098dff286a198
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-05 11:14:14 +0100

    Uploadet Basic Opgave. 1

---
+++ Basic1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Basic1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tal 1:");
+            int nr1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Tal 2:");
+            int nr2 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Tal 3:");
+            int nr3 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine(AddAndMultiply(nr1, nr2, nr3));
+            Console.ReadLine();
+        }
+        public static int AddAndMultiply(int nr1, int nr2, int nr3)
+        {
+            int equalAddedAndMultiplied = (nr1 + nr2) * nr3;
+            return equalAddedAndMultiplied;
+        }
+    }
+}
