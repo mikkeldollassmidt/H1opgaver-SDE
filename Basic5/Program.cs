8a1b4901c4a4372245f68c71e3ed28d52246dfd4
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-05 12:47:22 +0100

    Uploadet Basic Opgave 5

---
+++ Basic5/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Basic5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // User interaction
+            Console.WriteLine("Write first number:");
+            int nr1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Write second number:");
+            int nr2 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Write third number:");
+            int nr3 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            // write and use ModuloOperations method to calculate end result.
+            Console.WriteLine("= " + ModuloOperations(nr1, nr2, nr3));
+            Console.ReadLine();
+        }
+
+        public static int ModuloOperations(int nr1, int nr2, int nr3)
+        {
+            // Modulo of the first two.
+            int firstNumberModulo = nr1 % nr2;
+
+            // Modulo of first modulo and last number given.
+            int lastNumberModulo = firstNumberModulo % nr3;
+
+            // Return end result.
+            return lastNumberModulo;
+        }
+    }
+}
