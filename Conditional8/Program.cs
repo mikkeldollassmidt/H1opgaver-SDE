eaef432ae2b8b1028562ee4d1855def11b31f1eb
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-06 12:50:33 +0100

    Uploadet Conditional Opgave 8

---
+++ Conditional8/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Conditional8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // User interaction:
+                Console.WriteLine("Write a year (yyyy):");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                // Trying to use IfYearIsLeap method to check if userInput (year) is a leap year.
+                Console.WriteLine(IfYearIsLeap(year));
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                // Show exception if exception is catched.
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+        public static bool IfYearIsLeap(int year)
+        {
+            // Using DateTime.IsLeapYear method to check if year is 2000, 2004, 2008.... And so on.
+            if (DateTime.IsLeapYear(year))
+            {
+                // If true, return true.
+                return true;
+            }
+             else
+            {
+                // Else return false.
+                return false;
+            }
+        }
+    }
+}
