550b35a6a92ffab7212a6d3326b33534a48f4514
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-06 12:39:52 +0100

    Uploadet Conditional Opgave 7

---
+++ Conditional7/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Conditional7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // User interaction:
+                Console.WriteLine("Write a number:");
+                double inputNumber = Convert.ToDouble(Console.ReadLine());
+                Console.Clear();
+
+                // Try method and return a result.
+                Console.WriteLine(PositiveNegativeOrZero(inputNumber));
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                // Catch exception and handle.
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+        public static string PositiveNegativeOrZero(double inputNumber)
+        {
+            // Check if number is larger than 0 and return positive.
+            if (inputNumber > 0)
+            {
+                return "Positive";
+            }
+            // Check if number is equal to 0 return zero.
+            else if (inputNumber == 0)
+            {
+                return "Zero";
+            // Else return negative, then it must be negative.
+            } else
+            {
+                return "Negative";
+            }
+        }
+    }
+}
