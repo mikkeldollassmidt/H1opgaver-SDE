8f72cb2ca32ea4ecbdf680da1dccff320d0c9879
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-09 11:01:44 +0100

    Uploadet Loops Opgave 69

---
+++ Loops6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Loops6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInputString = "klasdmlasmdl##MikkelOtteDollasSmidt##lkmaslkmdasldmk";
+
+            Console.WriteLine(ExtractString(userInputString));
+            Console.ReadLine();
+        }
+
+        static string ExtractString(string userInputString)
+        {
+            // Standard variable for extractedText
+            string extractedText = null;
+
+            // Find index of hashtags. Place a "keypoint" if first hashtags are found like the prefix in the code. IndexOf
+            int start = userInputString.IndexOf("##");
+            int end = userInputString.LastIndexOf("##");
+
+            // Is hashtags found?
+            if (start != -1 && end != -1 && start != end)
+            {
+                // extractedText string for putting in text between hashtags to extract to users console.
+                extractedText = userInputString.Substring(start + 2, end - start - 2);
+                return extractedText;
+            }
+            else
+            {
+                // Found no text inbetween hashtags, show error.
+                return "No text in hashtags";
+            }
+        }
+    }
+}
