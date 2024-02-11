86ada4c49c4a2520fe6d2ca7d957496b233a8818
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-11 21:44:44 +0100

    Uploadet Strings Opgave 2

---
+++ Strings2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Strings2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // User interaction, creating variables and putting value in them.
+            Console.WriteLine("Write a word and check if it's a palindrome (in lower case.):");
+
+            // Creating a temp string to make all userInput to lower.
+            string userInputTemp = Console.ReadLine();
+            string userInput = userInputTemp.ToLower();
+            Console.Clear();
+
+            // Using method to test if userInput is palindrome.
+            Console.WriteLine(IsPalindrome(userInput));
+            Console.ReadLine();
+        }
+        public static bool IsPalindrome(string userInput)
+        {
+            string userInputReversed = "";
+
+            // For loop to reverse userInput. - 1, because of index is 0 / First letter will be on index 0.
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                // Write reversed into userInputReversed.
+                userInputReversed += userInput[i];
+            }
+
+            // Check if user input reversed and user input is the same (isPalindrome) and return bool.
+            if (userInputReversed == userInput)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
