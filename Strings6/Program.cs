4e4ca4fd45349975d15e2f3d1037bc5f5780e061
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-12 09:35:00 +0100

    Uploadet Strings Opgave 6

---
+++ Strings6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Strings6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // User interaction
+            Console.WriteLine("Write a sentence (Example: Hello my name is..):");
+            string userInput = Console.ReadLine();
+            Console.Clear();
+
+            // Using method to revert words order.
+            Console.WriteLine(RevertWordsOrder(userInput));
+
+        }
+        public static string RevertWordsOrder(string userInput)
+        {
+            // Using Libraries to split and reverse string in a array. After splitting and reversing, we're joining the array into a string again.
+            return string.Join(" ", userInput.Split(' ').Reverse());
+        }
+    }
+}
