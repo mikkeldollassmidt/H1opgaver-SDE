bed6ede630d2ca644eac61631abb1fa1cdad6486
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-12 08:21:34 +0100

    Uploadet Strings Opgave 4

---
+++ Strings4/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Strings4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // User interaction, creating variables and putting value in them from user input.
+            Console.WriteLine("Write a word or sentence:");
+            string userInput = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(StringInReverseOrder(userInput));
+            Console.ReadLine();
+        }
+        public static string StringInReverseOrder(string userInput)
+        {
+            // Standard variable for reversed userinput.
+            string userInputReversed = null;
+
+            // For loop to reverse userInput. - 1, because of index is 0 / First letter will be on index 0.
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                // Write reversed into userInputReversed.
+                userInputReversed += userInput[i];
+            }
+
+            // Return userInput in reversed order.
+            return userInputReversed;
+        }
+    }
+}
