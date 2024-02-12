a29023f8ddb78eb46ce387f801440c7cd16e3d85
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-12 12:20:00 +0100

    Uploadet Strings Opgave 7

---
+++ Strings7/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Strings7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // user interaction, creating variables and putting user input inside of them.
+            Console.WriteLine("Write a sentence:");
+            string userWordInput = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Check occurrences of word:");
+            string userLookupWord = Console.ReadLine();
+            Console.Clear();
+
+            // Use method to get count of occurrences in userWordinput of userLookupWord.
+            Console.WriteLine(HowManyOccurrences(userWordInput, userLookupWord));
+        }
+        public static int HowManyOccurrences(string userWordInput, string userLookupWord)
+        {
+            // Int to split where the userLookupWord is and count length.
+            int countOccurrences = userWordInput.Split(userLookupWord).Length - 1;
+
+            // Return occurrences as int.
+            return countOccurrences;
+        }
+    }
+}
