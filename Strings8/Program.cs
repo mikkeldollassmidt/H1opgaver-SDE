764d5fb76fe3e64ae45061d7149ef8aa923176e1
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-12 12:43:58 +0100

    Uploadet Strings Opgave 8

---
+++ Strings8/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Strings8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // user interaction and creating and putting in variables with user input.
+            Console.WriteLine("Write a sentence:");
+            string userInput = Console.ReadLine();
+            Console.Clear();
+
+            // Using method to sortCharacters to descended.
+            Console.WriteLine(SortCharactersDescending(userInput));
+        }
+        public static string SortCharactersDescending(string userInput)
+        {
+            // Using this method it makes userInput into chars and using library OrderByDescending char to char into array and writes out in a string with new string. Using lambda to calculate inline.
+            return new string(userInput.OrderByDescending(c => c).ToArray());
+        }
+    }
+}
