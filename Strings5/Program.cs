ac248a82c880e9de783ea90a0a80afa4d7f5c0f2
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2024-02-12 08:31:43 +0100

    Uploadet Strings Opgave 5

---
+++ Strings5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Strings5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // User interaction, creating variable and putting userinput value in it.
+            Console.WriteLine("Please write a sentence (Example: Hello my name is..):");
+            string userInput = Console.ReadLine();
+            Console.Clear();
+
+            // Using method to find number of words with the userinput
+            Console.WriteLine(NumberOfWords(userInput));
+            Console.ReadLine();
+        }
+        public static int NumberOfWords(string userInput)
+        {
+            // Split in a string array when user has used the space character. Every word before and after space gets put in a string array called userInputSplitSpaces.
+            string[] userInputSplitSpaces = userInput.Split(' ');
+
+            // Count how many values in array.
+            int splitCount = userInputSplitSpaces.Count();
+
+            // Return value.
+            return splitCount;
+        }
+    }
+}
